029cdead476ed9bc605022632bec328d81630871
Paul Herera <linus_pagan@hotmail.com>
2012-12-30 19:07:32 -0600

    Completed doors

--- spec/OneHundredDoorsSpec.js
+++ spec/OneHundredDoorsSpec.js
@@ -2,6 +2,9 @@ describe("100 Doors", function(){
 
     var doors;
 
+    var closed = false;
+    var opened = true;
+
     beforeEach(function(){
         doors = new OneHundredDoors();
     });
@@ -28,37 +31,97 @@ describe("100 Doors", function(){
         doors.step();
         doors.step();
 
-        for(var x = 0; x < 100; x++) {
-            if ((x + 1) % 2 === 0) {
-                expect(doors.isOpen(x)).toEqual(false);
-            }
-            else {
-                expect(doors.isOpen(x)).toEqual(true);
-            }
+        for(var i = 0; i < 100; i++) {
+            expectOddIndexToBe(closed, i);
         }
     });
+
+    var expectOddIndexToBe = function(b, x) {
+        if ((x + 1) % 2 === 0) {
+            expect(doors.isOpen(x)).toEqual(b);
+        }
+        else {
+            expect(doors.isOpen(x)).toEqual(!b);
+        }
+    };
 
     it("should toggle every third door on three steps", function () {
         doors.step();
         doors.step();
         doors.step();
 
-        for(var x = 0; x < 100; x++) {
+        for(var i = 0; i < 100; i++) {
 
-            var place = (x + 1);
+            var place = (i + 1);
 
-            if ((place % 2 === 0) && (place % 3 === 0)) {
-                expect(doors.isOpen(x)).toEqual(true);
+            if (place % 3 === 0) {
+                expectOddIndexToBe(opened, i);
             }
-            else if (place % 2 === 0) {
-                expect(doors.isOpen(x)).toEqual(false);
-            }
-            else if ((place % 2 !== 0) && (place % 3 === 0)) {
-                expect(doors.isOpen(x)).toEqual(false);
-            }
-            else if ((place % 2 !== 0)) {
-                expect(doors.isOpen(x)).toEqual(true);
+            else {
+                expectOddIndexToBe(closed, i);
             }
         }
+    });
+});
+
+describe("Numbers function", function(){
+    var getBool = function(stepNum, index){
+
+        var returnValue = false;
+
+        var place = (index + 1);
+
+        for(var stepCount = 0; stepCount <= stepNum; stepCount++){
+            if ((stepNum >= stepCount) && (place % (stepCount + 1)) === 0) {
+                returnValue = !returnValue;
+            }
+        }
+
+        return returnValue;
+    };
+
+    it("step 0", function () {
+        var stepNum = 0;
+
+        expect(getBool(stepNum, 0)).toEqual(true);
+        expect(getBool(stepNum, 1)).toEqual(true);
+        expect(getBool(stepNum, 2)).toEqual(true);
+        expect(getBool(stepNum, 3)).toEqual(true);
+        expect(getBool(stepNum, 4)).toEqual(true);
+        expect(getBool(stepNum, 5)).toEqual(true);
+        expect(getBool(stepNum, 6)).toEqual(true);
+        expect(getBool(stepNum, 7)).toEqual(true);
+        expect(getBool(stepNum, 8)).toEqual(true);
+        expect(getBool(stepNum, 9)).toEqual(true);
+    });
+
+    it("step 1", function () {
+        var stepNum = 1;
+
+        expect(getBool(stepNum, 0)).toEqual(true);
+        expect(getBool(stepNum, 1)).toEqual(false);
+        expect(getBool(stepNum, 2)).toEqual(true);
+        expect(getBool(stepNum, 3)).toEqual(false);
+        expect(getBool(stepNum, 4)).toEqual(true);
+        expect(getBool(stepNum, 5)).toEqual(false);
+        expect(getBool(stepNum, 6)).toEqual(true);
+        expect(getBool(stepNum, 7)).toEqual(false);
+        expect(getBool(stepNum, 8)).toEqual(true);
+        expect(getBool(stepNum, 9)).toEqual(false);
+    });
+
+    it("step 2", function () {
+        var stepNum = 2;
+
+        expect(getBool(stepNum, 0)).toEqual(true);
+        expect(getBool(stepNum, 1)).toEqual(false);
+        expect(getBool(stepNum, 2)).toEqual(false);
+        expect(getBool(stepNum, 3)).toEqual(false);
+        expect(getBool(stepNum, 4)).toEqual(true);
+        expect(getBool(stepNum, 5)).toEqual(true);
+        expect(getBool(stepNum, 6)).toEqual(true);
+        expect(getBool(stepNum, 7)).toEqual(false);
+        expect(getBool(stepNum, 8)).toEqual(false);
+        expect(getBool(stepNum, 9)).toEqual(false);
     });
 });
